94fe9393a45ba21bd36b77bc4227c310e1dd66ff
Matthew Stone <generalms@hotmail.com>
2016-07-07 20:31:08 -0500

    Changed contact model

---
+++ fstmobile/migrations/0004_auto_20160708_0116.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('fstmobile', '0003_auto_20160708_0113'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='contact',
+            name='created',
+            field=models.DateTimeField(default=datetime.datetime(2016, 7, 8, 1, 16, 28, 78698), verbose_name=b'auto_now_add=true'),
+        ),
+    ]
--- fstmobile/models.py
+++ fstmobile/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class Contact(models.Model):
 	name = models.CharField(max_length=255);
 	number = models.CharField(max_length=255);
-	created = models.DateTimeField('auto_now_add=True');
+	created = models.DateTimeField('auto_now_add=true',editable=True,default=datetime.datetime.now());
+
+	def __str__(self):
+		return self.name
+
+
 
