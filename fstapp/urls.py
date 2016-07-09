75898e40dd78b5307e64b4626caab4693bc973d2
Matthew Stone <generalms@hotmail.com>
2016-07-09 16:10:32 -0500

    Added homepage

--- fstapp/urls.py
+++ fstapp/urls.py
@@ -4,9 +4,10 @@ from fstmobile import views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'fstapp.views.home', name='home'),
+     
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$','fstmobile.views.index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^contacts/$', views.ListContacts.as_view()),
 
--- fstmobile/views.py
+++ fstmobile/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from fstmobile.serializers import ContactSerializer
 from fstmobile.models import Contact
 # Create your views here.
+
+
+
+def index(request):
+	return render_to_response('fstmobile/index.html')
+
 
 
 class ListContacts(APIView):
@@ -14,4 +21,7 @@ class ListContacts(APIView):
 		contacts = Contact.objects.all()
 		serializer = ContactSerializer(contacts,many=True)
 		return Response(serializer.data)
+
+
+
 
