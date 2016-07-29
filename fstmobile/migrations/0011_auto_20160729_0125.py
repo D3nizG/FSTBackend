62dd03494e37480cc2a5bb3c156685aa960de1af
Matthew Stone <generalms@hotmail.com>
2016-07-29 00:41:02 -0500

    Merged with repo and added more places

--- fstmobile/migrations/0011_auto_20160729_0125.py
+++ fstmobile/migrations/0011_auto_20160729_0125.py
@@ -12,17 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Scholarship',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255)),
-                ('description', models.CharField(max_length=400)),
-                ('detail', models.TextField()),
-                ('image_url', models.CharField(max_length=255)),
-                ('created', models.DateTimeField(default=datetime.datetime(2016, 7, 29, 1, 25, 28, 999522), verbose_name=b'auto_now_add=true', editable=False)),
-            ],
-        ),
         migrations.AlterField(
             model_name='contact',
             name='created',
--- fstmobile/models.py
+++ fstmobile/models.py
@@ -15,6 +15,9 @@ class FAQ(models.Model):
 	question = models.CharField(max_length=255);
 	answer = models.CharField(max_length=255);
 
+	def __str__(self):
+		return self.question
+
 class News(models.Model):
 	title = models.CharField(max_length=255)
 	description = models.CharField(max_length=400)
